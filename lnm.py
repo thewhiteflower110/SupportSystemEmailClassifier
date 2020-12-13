6321191666076478638d72eb7d3a869be3d80c52
Juhi Kamdar <juhikamdar110@gmail.com>
2020-12-14 00:51:55 +0530

    Fixed bugs

--- lnm.py
+++ lnm.py
@@ -3,12 +3,19 @@ def get_category(logits,t2):
     dicti_bert = {0:"Transfer",1:"Retirement",2:"MDU"}
     dicti_lda = {0:"Transfer",1:"MDU",2:"Retirement"}
 
-    for bert,lda in logits,t2:
+ 
+    for i in logits:
+      if isinstance(i,float):
+        logits=[logits]
+        t2=[t2]
+
+    for bert,lda in zip(logits,t2):
         x=bert.index(min(bert)) #crossentropy loss is outputted in the tensor
-        y=bert.index(max(lda))
+        y=lda.index(max(lda))
     
         #assuming the order of the topics are same in bert and lda
         if dicti_bert[x]==dicti_lda[y]:
             l.append(dicti_bert[x])
         else:
-            l.append(dicti_lda[y])            
+            l.append(dicti_lda[y])  
+    return l          
