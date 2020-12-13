8c97f493e698c4e8080d428f9813156ff0a723ee
Meet Gandhi <meet.gandhi1415@gmail.com>
2020-12-13 20:36:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import zipfile
 import io
+import os
 import pathlib
 from flask import Flask
 from flask import request
 from flask import json
 import main
 
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+
+
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "C:/Users/Dell/Desktop/New folder/SupportSystemEmailClassifier-main/Zip_File"
+
 @app.route("/", methods=['POST'])
 def execute():
         #method = request.form.get('method')
@@ -18,6 +27,13 @@ def execute():
         response = main.predict(app,zipfile,bert=str_to_bool(bert),lda=str_to_bool(lda),combined=str_to_bool(combined))
         return response
 
+@app.route("/train", methods=['POST'])
+def train_model():
+        filename= request.form.get('filename')
+        data = request.form.get('data')
+        response = main.train(filename,data)
+        return response
+
 def str_to_bool(s):
         if s=='True':
                 return True
@@ -25,6 +41,16 @@ def str_to_bool(s):
                 return False
         else:
                 return 'valueError'
+
+	
+@app.route('/uploader', methods = ['POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		filename = secure_filename(f.filename)
+		f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return 'file uploaded successfully'
+
 
 if __name__=='__main__':
         app.run()
